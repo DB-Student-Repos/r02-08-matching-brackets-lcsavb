f21df30987f411c1725909c4c5c256ada2203086
Lucas Barros <lcsavb@gmail.com>
2024-03-21 21:47:02 +0100

    not need for match

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,8 @@ use regex::Regex;
 pub fn brackets_are_balanced(string: &str) -> bool {
     let mut haystack = String::new();
     for char in string.chars() {
-        match char {
-            '[' | ']' | '{' | '}' | '(' | ')' => haystack.push(char),
-            _ => {}
+        if let '[' | ']' | '{' | '}' | '(' | ')' = char {
+            haystack.push(char);
         }
     }
     
