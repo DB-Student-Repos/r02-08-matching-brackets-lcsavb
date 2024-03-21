faf24ad05bf813eb8ad7eb0e3f0fe3e717dae3ae
Lucas Barros <lcsavb@gmail.com>
2024-03-21 21:37:21 +0100

    done

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,19 @@
+use regex::Regex;
+
 pub fn brackets_are_balanced(string: &str) -> bool {
-    unimplemented!("Check if the string \"{string}\" contains balanced brackets");
+    let mut haystack = String::new();
+    for char in string.chars() {
+        match char {
+            '[' | ']' | '{' | '}' | '(' | ')' => haystack.push(char),
+            _ => {}
+        }
+    }
+    
+    let re = Regex::new(r"\(\)|\[\]|\{\}").unwrap();
+    while re.is_match(&haystack) {
+        haystack = re.replace_all(&haystack, "").to_string();
+    }
+
+    haystack.is_empty()
 }
+
